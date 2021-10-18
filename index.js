3d283743314219f617aeb232eb542cd5d347d90a
Gregory Banks <gbanks1438@gmail.com>
2021-10-17 22:10:17 -0400

    commit tree 27dbc57848b5b2a1351340a5e55deed657d172cc

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("H1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Greg is the champion";
